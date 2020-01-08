f566e4d6c8109f23361bb0ac5f1101869282cff5
luciesindylkova <zbr19001@byui.edu>
2020-01-08 16:40:10 +0100

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+
+let d = new Date();
+let y = d.getFullYear();
+document.getElementById("currentYear").textContent = y;
+
+const lm = document.lastModified;
+document.getElementById("lastMod").textContent = lm;
