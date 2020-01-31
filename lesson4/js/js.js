3d60ce227a56c50fa90bbac3baac970bffc903ec
luciesindylkova <zbr19001@byui.edu>
2020-01-31 22:37:42 +0100

    cross added

--- lesson4/js/js.js
+++ lesson4/js/js.js
@@ -7,5 +7,14 @@ document.getElementById("today").textContent = (m+1) + "/" + d + "/" + y;
 
 function toggleMenu() {
     
+    menuButton.classList.toggle("hide");
+    if(menuButton.classList.contains("hide")) {
+        menuButton.innerHTML = "&Cross;";
+    }
+    else {
+        menuButton.innerHTML = "&#9776;";
+    }
+
     document.getElementById("primaryNav").classList.toggle("hide");
+
 } 
