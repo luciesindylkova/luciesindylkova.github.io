563daf64d9d243e2462c74a1296f2d5bc8bb695d
luciesindylkova <zbr19001@byui.edu>
2020-02-14 15:09:25 +0100

    lesson6 first draft

---
+++ lesson6/js/js.js
@@ -0,0 +1,33 @@
+
+
+let date = new Date();
+let y = date.getFullYear();
+let m = date.getMonth();
+let d = date.getDate();
+
+document.getElementById("today").textContent = (m+1) + "/" + d + "/" + y;
+
+function toggleMenu() {
+    
+    menuButton.classList.toggle("hide");
+    if(menuButton.classList.contains("hide")) {
+        menuButton.innerHTML = "&Cross;";
+    }
+    else {
+        menuButton.innerHTML = "&#9776;";
+    }
+
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+} 
+
+let day = date.getDay();
+
+
+const announcement = document.querySelector('.announcement');
+if(day == 5){
+    announcement.style.display = 'block';
+} else {
+    announcement.style.display = 'none';
+}
+
