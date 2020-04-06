55e7cc7c3c27dbb3aeb509342556f5d0ea045d6d
luciesindylkova <zbr19001@byui.edu>
2020-04-06 20:29:52 +0200

    commit

--- salmon_river/js/js.js
+++ salmon_river/js/js.js
@@ -11,3 +11,22 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 
 } 
+
+// Weather
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=9540be0505c3be9aad7b3b1fc611de04&units=imperial"
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+   
+
+    document.getElementById('current').innerHTML=weatherInfo.weather[0].main;
+    document.getElementById('temp').innerHTML=weatherInfo.main.temp + "&deg;F";
+    document.getElementById('humidity').innerHTML=weatherInfo.main.humidity + "%";
+    document.getElementById('speed').innerHTML=weatherInfo.wind.speed + "mph";
+
+    var t = weatherInfo.main.temp;
+    var s = weatherInfo.wind.speed;
+
+});
